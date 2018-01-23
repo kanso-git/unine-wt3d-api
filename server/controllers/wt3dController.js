7fb21cb67b76431460c3afff8fd96536dca88e0c
kanso <abdallah.kanso@gmail.com>
2018-01-23 17:10:43 +0100

    add the wt3d model continue add seed

--- server/controllers/wt3dController.js
+++ server/controllers/wt3dController.js
@@ -14,7 +14,26 @@ exports.wt3d_list = async(req, res) => {
     res.status(400).send(e);
   }
 };
+const buildings = [
+  {
+    name: 'Bellevaux51',
+    buildingIds: [2],
+    buildingRouteUrl: '/uninecampusbel51/NAServer/Route/solve',
+    serviceOffices: [{code: 'GA22', value: "Centrale d'achats produits chimiques"},
+      {code: 'GA30', value: 'Neuchâtel platform of analytical chemestry'},
+      {code: 'GA4-GA8-GA10', value: 'Atelier de construction mécanique'}],
+    secretarias: [{code: 'GE16', value: 'Institut de chimie'}, {code: 'GD16', value: 'Institut de physique'}],
+    elevators: ['(GA57)', '(GB62)', '(GC57)', '(GD53)', '(GE53)'],
+    stairs: ['(GA56)', '(GB54)', '(GA55)', '(GB52)', '(GC55)', '(GD50)', '(GE54)', '(GB53)'],
+    wcs: ['(GA52)', '(GA52a)', '(GA52b)', '(GA53a)', '(Ga57)',
+      '(GB55)', '(GB56)', '(GB57)', '(GB63)', '(GC53)', '(GC56)',
+      '(GC58)', '(GD51)', '(GD52)', '(GD57)', '(GE50)', '(GE51)', '(GE52)'],
+    otherTabs: [],
+    buildingRouteExceptions: [],
+    buildingRouteExclud: []
+  }
 
+];
 const localTypes = [{
   _id: new ObjectID(),
   name: 'Bibliothèques',
